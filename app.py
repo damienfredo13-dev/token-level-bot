599bdfe3d41931f4a98d92b1b9c529cdfcb7bf0d
damienfredo13-dev <damienfredo13@gmail.com>
2026-09-10 14:17:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -379,7 +379,8 @@ app.add_handler(CommandHandler("token", token))
 app.add_handler(CommandHandler("profil", profil))
 app.add_handler(CommandHandler("gagne", gagne))
 app.add_handler(CommandHandler("perdu", perdu))
-app.add_handler(CommandHandler("canalid", canal_id))
+app.add_handler(CommandHandler("perdu", perdu))
+app.add_handler(ChannelPostHandler(detecter_canal))
 
 app.add_handler(
     MessageHandler(
