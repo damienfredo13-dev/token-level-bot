b83e926468aff1d13beed34eb4c961d9b0b48d93
damienfredo13-dev <damienfredo13@gmail.com>
2026-09-12 13:44:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1002,12 +1002,12 @@ async def recevoir_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         pari_securise = html.escape(pari)
 
-  message_bet = (
-    "🚨🔴 <b>BET EN LIVE</b> 🔴🚨\n\n"
-    f"⚽ {pari_securise}\n"
-    f"🎯 Cote : @{cote:.2f}\n\n"
-    "🪙 On essaie de monter les Tokens ! 🚀🔥"
-)
+        message_bet = (
+            "🚨🔴 <b>BET LIVE</b> 🔴🚨\n\n"
+            f"⚽ <b>{pari_securise}</b>\n"
+            f"🎯 <b>Cote : @{cote:.2f}</b>\n\n"
+            "🪙 <b>On essaie de monter les tokens !</b> 🚀🔥"
+        )
 
         await context.bot.send_message(
             chat_id=CHANNEL_ID,
